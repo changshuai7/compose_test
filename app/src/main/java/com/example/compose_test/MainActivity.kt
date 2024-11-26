8f94a6a665dcfb540dfb7841e73bf3fecf549a11
changshuai01 <changshuai01@58.com>
2024-11-26 13:33:38 +0800

    [add] add firstBaseLineToTop

--- app/src/main/java/com/example/compose_test/MainActivity.kt
+++ app/src/main/java/com/example/compose_test/MainActivity.kt
@@ -4,50 +4,71 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.lerp
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.compose_test.components.ColumnLazy
 import com.example.compose_test.components.ColumnListView
+import com.example.compose_test.components.ComponentBaseLineToTop
 import com.example.compose_test.components.ComponentScaffold
 import com.example.compose_test.ui.theme.Compose_testTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             Compose_testTheme {
-//                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-//                    Greeting(
-//                        name = "Android",
-//                        modifier = Modifier.padding(innerPadding)
-//                    )
-//                }
-//                ComponentScaffold()
-//                ColumnLazy()
-                ColumnListView()
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    topBar = {
+                        TopAppBar(
+                            title = {
+                                Text(text = "Hello Compose")
+                            }
+                        )
+                    }
+                ) { padding ->
+                    ComposeBodyContent(modifier = Modifier.padding(padding))
+                }
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Compose_testTheme {
-        Greeting("Android")
+fun ComposeBodyContent(modifier: Modifier = Modifier) {
+    Box(modifier = modifier) {
+        Column {
+            Box(
+                modifier = Modifier
+                    .background(Color.Red)
+                    .fillMaxWidth()
+                    .height(1.dp)
+            )
+            ComponentBaseLineToTop()
+        }
     }
 }
---
+++ app/src/main/java/com/example/compose_test/components/ComponentCustomView.kt
@@ -0,0 +1,42 @@
+package com.example.compose_test.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.FirstBaseline
+import androidx.compose.ui.layout.layout
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+
+fun Modifier.firstBaseLineToTop(
+    firstBaseLineToTop: Dp
+): Modifier {
+    return this.then(
+        other = this.layout { measurable, constraints ->
+            val placeable = measurable.measure(constraints)
+            val baseLine = placeable[FirstBaseline]
+            val diff = firstBaseLineToTop.roundToPx() - baseLine
+            layout(placeable.width, placeable.height + diff) {
+                placeable.placeRelative(0, diff)
+            }
+        }
+    )
+}
+
+
+@Preview
+@Composable
+fun ComponentBaseLineToTop() {
+    Text(
+        text = "Hello World",
+        modifier = Modifier
+            .background(Color.Red)
+            .firstBaseLineToTop(50.dp)
+    )
+}
